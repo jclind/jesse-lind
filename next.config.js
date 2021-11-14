3697457c83800cada21133ffa29488a5348f7584
Jesse Lind <Jesseclind@gmail.com>
2021-11-14 15:58:24 -0500

    Cleaning up styles component, giving each style group a folder

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
+
+module.exports = withPlugins([[optimizedImages]])

1ce98deded19f3009afe25746535ce59798ed003
Jesse Lind <Jesseclind@gmail.com>
2021-11-16 09:18:21 -0500

    No longer adding loading page, also proloading fonts clears #8

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,70 +12,40 @@ import '../styles/util/animations.css'
 import '../styles/404/404.css'
 import '../styles/loading-page.css'
 
-import JLMainLogo from '../components/images/JLMainLogo'
 import Layout from '../components/Layout'
 
 import { AnimatePresence, motion } from 'framer-motion'
 import BarLoader from 'react-spinners/BarLoader'
 
 function MyApp({ Component, pageProps, router }) {
-    const [loading, setLoading] = useState(false)
-
-    useEffect(() => {
-        setLoading(true)
-        const handleStart = () => {
-            setLoading(true)
-        }
-        const handleComplete = () => {
-            setTimeout(() => {
-                setLoading(false)
-            }, 1500)
-        }
-        router.events.on('hashChangeStart', () => {
-            handleStart()
-        })
-        router.events.on('hashChangeComplete', () => {
-            handleComplete()
-        })
-    }, [router])
-
-    return (
-        <>
-            <Layout>
-                <AnimatePresence>
-                    <motion.div
-                        key={router.route}
-                        initial='pageInitial'
-                        animate='pageAnimate'
-                        exit='pageExit'
-                        variants={{
-                            pageInitial: {
-                                opacity: 0,
-                            },
-                            pageAnimate: {
-                                opacity: 1,
-                            },
-                            pageExit: {
-                                opacity: 0,
-                            },
-                        }}
-                        transition={{ duration: 0.2 }}
-                    >
-                        {!loading ? (
-                            <Component
-                                {...pageProps}
-                                jlMainLogo={<JLMainLogo />}
-                            />
-                        ) : (
-                            <div className='loader'>
-                                <BarLoader color={'#ff7c7c'} size={20} />
-                            </div>
-                        )}
-                    </motion.div>
-                </AnimatePresence>
-            </Layout>
-        </>
-    )
+  return (
+    <>
+      <Layout>
+        <AnimatePresence>
+          <motion.div
+            key={router.route}
+            initial='pageInitial'
+            animate='pageAnimate'
+            exit='pageExit'
+            variants={{
+              pageInitial: {
+                opacity: 0,
+              },
+              pageAnimate: {
+                opacity: 1,
+              },
+              pageExit: {
+                opacity: 0,
+              },
+            }}
+            transition={{ duration: 0.2 }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,10 @@ const Page = props => (
   <Html>
     <Head>
       <link rel='shortcut icon' href='/static/favicon.ico' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap'
+        rel='stylesheet'
+      ></link>
     </Head>
     <body>
       <Main />

3eb164c3a1f18ff52e09b4bd054e12ee7f14165f
Jesse Lind <Jesseclind@gmail.com>
2021-11-17 18:06:31 -0500

    Switching loading state for test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,7 @@ import Loading from '../components/Loading'
 import { AnimatePresence, motion } from 'framer-motion'
 
 function MyApp({ Component, pageProps, router }) {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     console.log('first load of website')

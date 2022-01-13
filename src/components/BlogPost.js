5c700b01c487c5d530f03cee53a6e5d1378480dc
Jesse Lind <Jesseclind@gmail.com>
2022-01-13 17:13:13 -0500

    feat: add render for post link if it exists in the post data and trim the url before showing in post

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -3,11 +3,36 @@ import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 // import '../styles/components/blog-post.css'
 import { formatDate } from '../util/formatDate'
 import BlogPostLogo from './BlogPostLogo'
+import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+const renderOption = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: node => {
+      return (
+        <img
+          src={`https:${node.data.target.fields.file.url}`}
+          className='blog-img'
+        />
+      )
+    },
+    [BLOCKS.PARAGRAPH]: (node, children) => (
+      <p className='blog-paragraph'>{children}</p>
+    ),
+  },
+}
+
+const trimLink = url => {
+  const urlTrim = url.replace(/(^\w+:|^)\/\//, '')
+  const result = urlTrim.split('/')[0]
+  return result
+}
 
 const BlogPost = ({ post: { fields, sys } }) => {
   const content = documentToHtmlString(fields.content)
+  const link = fields.link ? fields.link : null
   const date = formatDate(new Date(sys.createdAt))
-
+  console.log(content)
   return (
     <article className='blog-post'>
       <div className='divider'>
@@ -22,11 +47,19 @@ const BlogPost = ({ post: { fields, sys } }) => {
           </div>
           {fields.title}
         </div>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: content,
-          }}
-        />
+        <div className='rich-content'>
+          {documentToReactComponents(fields.content, renderOption)}
+        </div>
+        {link && (
+          <a
+            href={link}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='post-link'
+          >
+            {trimLink(link)}
+          </a>
+        )}
       </div>
     </article>
   )

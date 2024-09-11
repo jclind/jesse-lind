0cf504af884a048c34c336b0819a4fce1c2f1cf2
Jesse Lind <Jesseclind@gmail.com>
2024-09-11 13:33:54 -0400

    update: add Pump Track to projects and fix various grammer and spelling discrepancies through the project descriptions

--- src/assets/data/projectData.js
+++ src/assets/data/projectData.js
@@ -2,15 +2,23 @@ export const projectData = [
   {
     path: '/projects-page/prepify.png',
     name: 'Prepify',
-    desc: 'A user-friendly recipe website focused on meal prep and saving money while cooking.',
+    desc: 'An intuitive, distraction-free recipe website focused on meal prep and saving money while cooking.',
     skills: ['React.js', 'Typescript', 'Sass', 'Firebase', 'MongoDB'],
     link: 'https://prepifymeals.com/',
     id: 1,
   },
   {
+    path: '/projects-page/pump-track.png',
+    name: 'Pump Track',
+    desc: 'Pump Track is a streamlined, user-friendly web app that offers a simple input system and clearly organized workout logs. Users can easily track their progress with charts, view past exercise details while typing, sort their workouts and much more.',
+    skills: ['React.js', 'Typescript', 'Sass', 'Firebase'],
+    link: 'https://pumptrack.netlify.app/',
+    id: 9,
+  },
+  {
     path: '/projects-page/bitworkout.png',
     name: 'BitWorkout',
-    desc: 'A gamified RPG like workout app to give added motivation to exercice. Users can earn coins/experience, purchase items with their rewards, create their own workouts, track their progress and much more.',
+    desc: 'A gamified RPG-like workout app to give added motivation to exercise. Users can earn coins/experience, purchase items with their rewards, create their own workouts, track their progress and much more.',
     skills: ['React.js', 'Cypress.io', 'Chart.js', 'Sass', 'Firebase'],
     link: 'https://bitworkout.netlify.app/',
     id: 8,
@@ -18,7 +26,7 @@ export const projectData = [
   {
     path: '/projects-page/jesselindphotography.png',
     name: 'Jesse Lind Photography',
-    desc: "This page is my personal photography portfolio website. I've always been fascinated with photography and capturing a moment, and over the years I've taken some pictures that I think are worthy of sharing.",
+    desc: "My personal photography portfolio website. I've always been fascinated with photography and capturing moments, and over the years, I've taken some pictures that I think are worthy of sharing.",
     skills: ['Gatsby.js', 'React.js', 'Sass', 'Contentful'],
     link: 'https://jesselindphotography.netlify.app/',
     id: 2,
@@ -26,7 +34,7 @@ export const projectData = [
   {
     path: '/projects-page/tridle.png',
     name: 'Tridle',
-    desc: 'A three letter Wordle game featuring themes, colorblind-mode and stat tracking.',
+    desc: 'A three letter Wordle game featuring themes, colorblind mode and stat tracking.',
     skills: ['React.js', 'Sass', 'Firebase'],
     link: 'https://tridle.netlify.app/',
     id: 4,
